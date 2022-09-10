feaece50f09fabc02b0d5944ccbb4ab793a030c9
Andreas Steffan <a.steffan@contentreich.de>
2022-09-10 19:14:17 +0200

    chore: Made it acutually work

--- lib/utils.bash
+++ lib/utils.bash
@@ -2,7 +2,6 @@
 
 set -euo pipefail
 
-# TODO: Ensure this is the correct GitHub homepage where releases can be downloaded for tfctl.
 GH_REPO="https://github.com/weaveworks/tf-controller"
 TOOL_NAME="tfctl"
 TOOL_TEST="tfctl --help"
@@ -19,6 +18,23 @@ if [ -n "${GITHUB_API_TOKEN:-}" ]; then
   curl_opts=("${curl_opts[@]}" -H "Authorization: token $GITHUB_API_TOKEN")
 fi
 
+# get_arch discovers the architecture for this system .
+get_arch() {
+  ARCH=$(uname -m)
+  case $ARCH in
+  armv5*) ARCH="armv5" ;;
+  armv6*) ARCH="armv6" ;;
+  armv7*) ARCH="arm" ;;
+  arm64) ARCH="arm64" ;; # TODO Fix to proper when M1 packages are available
+  aarch64) ARCH="arm64" ;;
+  x86) ARCH="386" ;;
+  x86_64) ARCH="amd64" ;;
+  i686) ARCH="386" ;;
+  i386) ARCH="386" ;;
+  esac
+  echo "$ARCH"
+}
+
 sort_versions() {
   sed 'h; s/[+-]/./g; s/.p\([[:digit:]]\)/.z\1/; s/$/.z/; G; s/\n/ /' |
     LC_ALL=C sort -t. -k 1,1 -k 2,2n -k 3,3n -k 4,4n -k 5,5n | awk '{print $2}'
@@ -31,7 +47,6 @@ list_github_tags() {
 }
 
 list_all_versions() {
-  # TODO: Adapt this. By default we simply list the tag names from GitHub releases.
   # Change this function if tfctl has other means of determining installable versions.
   list_github_tags
 }
@@ -41,8 +56,12 @@ download_release() {
   version="$1"
   filename="$2"
 
-  # TODO: Adapt the release URL convention for tfctl
-  url="$GH_REPO/archive/v${version}.tar.gz"
+  local operating_system="$(uname)" # | tr '[:upper:]' '[:lower:]')"
+  local arch="$(get_arch)"
+  local platform="${operating_system}_${arch}"
+
+  # https://stackoverflow.com/questions/48678152/how-to-detect-386-amd64-arm-or-arm64-os-architecture-via-shell-bash
+  url="$GH_REPO/releases/download/v${version}/${TOOL_NAME}_${platform}.tar.gz"
 
   echo "* Downloading $TOOL_NAME release $version..."
   curl "${curl_opts[@]}" -o "$filename" -C - "$url" || fail "Could not download $url"
@@ -61,7 +80,6 @@ install_version() {
     mkdir -p "$install_path"
     cp -r "$ASDF_DOWNLOAD_PATH"/* "$install_path"
 
-    # TODO: Assert tfctl executable exists.
     local tool_cmd
     tool_cmd="$(echo "$TOOL_TEST" | cut -d' ' -f1)"
     test -x "$install_path/$tool_cmd" || fail "Expected $install_path/$tool_cmd to be executable."
